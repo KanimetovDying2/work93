42667a119e3ac60c2989548309c9c39ae1515c6d
Baiteli <toplegionmainer@gmail.com>
2026-09-17 17:18:45 +0600

    feat: create first model and dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ArtistsModule } from './artists/artists.module.js';
 
 @Module({
-  imports: [],
+  imports: [ArtistsModule],
   controllers: [AppController],
   providers: [AppService],
 })
