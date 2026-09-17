ea85c841d98ea89980ae59236f0736d5c019e87a
Baiteli <toplegionmainer@gmail.com>
2026-09-17 18:17:28 +0600

    feat: finish work93 with configured project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ArtistsModule } from './artists/artists.module.js';
+import { AlbumsModule } from './albums/albums.module.js';
+import { TracksModule } from './tracks/tracks.module.js';
 
 @Module({
-  imports: [ArtistsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/work93api'),
+    ArtistsModule,
+    AlbumsModule,
+    TracksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
